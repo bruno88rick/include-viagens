14660603b1996d32d26991f2ad693a9fe10ff09c
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2023-11-06 18:21:55 -0300

    Initial Commit

---
+++ include-viagens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  include-viagens
+//
+//  Created by Bruno Oliveira on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ include-viagens/include_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  include_viagensApp.swift
+//  include-viagens
+//
+//  Created by Bruno Oliveira on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct include_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
